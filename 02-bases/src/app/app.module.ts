130f735e7dbc3c3a818f185f6162faf91c44da2f
TomasRivetta <totorivetta@gmail.com>
2024-11-07 17:24:28 -0300

    seccion 4 terminada

--- 02-bases/src/app/app.module.ts
+++ 02-bases/src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { CounterModule } from './counter/counter.module';
+import { HeroesModule } from './heroes/heroes.module';
+
 import { AppComponent } from './app.component';
-import { CounterComponent } from './counter/counter.component';
 
 @NgModule({
-  declarations: [AppComponent, CounterComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, CounterModule, HeroesModule],
   providers: [],
   bootstrap: [AppComponent],
 })
